642f43dc6bc97c8cf4db6c010606e2c294526ee4
Dewan Mizanur Rahman <dewan.mizanur911@gmail.com>
2021-10-02 00:16:43 +0600

    value bug defined and solved

--- src/components/OrderReview/OrderReview.js
+++ src/components/OrderReview/OrderReview.js
@@ -1,10 +1,43 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
+import useProducts from '../../hooks/loadProducts';
+import useCart from '../../hooks/useCart';
+import { clearTheCart, deleteFromDb } from '../../utilities/fakedb';
+import Cart from '../Cart/Cart';
+import ReviewItem from '../ReviewItem/ReviewItem';
 
 const OrderReview = () => {
+    const [products] = useProducts();
+    const [cart, setCart] = useCart(products);
+    const history = useHistory();
+    
+    const handleOnRemove = key => {
+        const newItems = cart.filter(product => product.key !== key);
+        setCart(newItems);
+        deleteFromDb(key);
+    };
+
+    const handleOrderNow = () => {
+      history.push('/placeorder');
+      clearTheCart();
+    }
     return (
-        <div>
-            <h2>This is Order Review</h2>
+      <div className="shop-container">
+        <div className="product-container">
+          {cart.map((product) => (
+            <ReviewItem
+              key={product.key}
+              product={product}
+              handleOnRemove={handleOnRemove}
+            ></ReviewItem>
+          ))}
         </div>
+        <div className="cart-container">
+          <Cart cart={cart}>
+              <button onClick={handleOrderNow} className="btn-regular">Order Now</button>
+          </Cart>
+        </div>
+      </div>
     );
 };
 
--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -3,6 +3,7 @@ import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import { addToDb, getStoredCart } from '../../utilities/fakedb';
 import './Shop.css';
+import { Link } from 'react-router-dom';
 
 const Shop = () => {
     const [products, setProducts] = useState([]);
@@ -35,8 +36,19 @@ const Shop = () => {
         }
     }, [products])
 
-    const handleAddToCart = (product) => {
-        const newCart = [...cart, product];
+  const handleAddToCart = (product) => {
+    const exists = cart.find(pd => pd.key === product.key);
+    let newCart = [];
+    if (exists) {
+      // adding value if exists
+      const rest = cart.filter(pd => pd.key !== product.key);
+      exists.quantity = exists.quantity + 1;
+      newCart = [...rest, exists]
+    }
+    else {
+      product.quantity = 1;
+      newCart = [...cart, product];
+    }
         setCart(newCart);
         // save to local storage (for now)
         addToDb(product.key);
@@ -51,29 +63,35 @@ const Shop = () => {
     }
 
     return (
-        <>
-            <div className="search-container">
-                <input
-                    type="text"
-                    onChange={handleSearch}
-                    placeholder="Search Product" />
-            </div>
-            <div className="shop-container">
-                <div className="product-container">
-                    {
-                        displayProducts.map(product => <Product
-                            key={product.key}
-                            product={product}
-                            handleAddToCart={handleAddToCart}
-                        >
-                        </Product>)
-                    }
-                </div>
-                <div className="cart-container">
-                    <Cart cart={cart}></Cart>
-                </div>
-            </div>
-        </>
+      <>
+        <div className="search-container">
+          <input
+            type="text"
+            onChange={handleSearch}
+            placeholder="Search Product"
+          />
+        </div>
+        <div className="shop-container">
+          <div className="product-container">
+            {displayProducts.map((product) => (
+              <Product
+                key={product.key}
+                product={product}
+                handleAddToCart={handleAddToCart}
+              ></Product>
+            ))}
+          </div>
+          <div className="cart-container">
+            <Cart cart={cart}>
+              <Link to="/orders">
+                <button className="btn-regular">
+                                Review Order
+                </button>
+              </Link>
+            </Cart>
+          </div>
+        </div>
+      </>
     );
 };
 
